c1f283d3df0dd19ca1a6dca049a95224ceebd5e8
Nipun Mihimal <contact.nipunm@gmail.com>
2026-02-24 09:47:08 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LoanManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
